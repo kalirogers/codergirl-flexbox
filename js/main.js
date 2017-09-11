66f7982f60738796697503aab6ab2152be817c59
Kali Rogers <kalirogers@gmail.com>
2017-09-10 21:19:55 -0500

    Add javascript file with first lesson

---
+++ js/main.js
@@ -0,0 +1,25 @@
+console.log("This file is linked!");
+
+var teacher = "Professor McGonagall";
+var title = "Tranfiguration";
+var rating = [3.4, 5.0, 4.2];
+
+console.log(
+  "Teacher: " + teacher + "\n"+ "Department: " + title + "\n" + "Rating: " + rating
+);
+
+
+var avgRating = (3.4 + 5.0 + 4.2)/3;
+rdAvgRating = avgRating.toFixed(2);
+
+console.log("Average Rating: " + rdAvgRating);
+
+var studName = "Harry Potter";
+var studMajor = "Defense Against the Dark Arts";
+var studEmail = "harry@hogwarts.com";
+var studGpa = 3.7;
+var studCourses = "Defense 101";
+
+console.log(
+  "Name: " + studName + "\n"+ "Major: " + studMajor + "\n" + "Email: " + studEmail + "\nGPA: " + studGpa + "\nCourses: " + studCourses
+);
