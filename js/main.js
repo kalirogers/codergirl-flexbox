aed531639296fef9a006e7df1ed87c7baae0cd99
Kali Rogers <kalirogers@gmail.com>
2017-09-25 21:32:32 -0500

    Added javascript from lesson 1

--- js/main.js
+++ js/main.js
@@ -23,3 +23,13 @@ var studCourses = "Defense 101";
 console.log(
   "Name: " + studName + "\n"+ "Major: " + studMajor + "\n" + "Email: " + studEmail + "\nGPA: " + studGpa + "\nCourses: " + studCourses
 );
+
+function getRatingAvg(rating[]) {
+  var total = 0;
+  for(i=0, i<=rating.length, i++){
+    total = total + rating[i];
+  }
+  console.log(total/rating.length);
+}
+
+getRatingAvg(rating[1.5, 2.5, 3.5, 4.5, 7.0]);
