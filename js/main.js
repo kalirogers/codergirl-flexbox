6ee4f5fbbc81f4059774dae5d54df4dd1502d495
Kali Rogers <kalirogers@gmail.com>
2017-10-11 18:21:54 -0500

    added Javascript 3 project - class level/alert

--- js/main.js
+++ js/main.js
@@ -13,8 +13,6 @@ var rdAvgRating = avgRating.toFixed(2);
 
 var rateArray = [1.5, 2.5, 3.5, 4.5, 7.0];
 
-
-
 /*functions*/
 
 function getRatingAvg(rates) {
@@ -91,16 +89,134 @@ function filterDepartment(courseArray, department) {
 var newArray = filterDepartment(allCourses, "Transfiguration");
 console.log(newArray);
 
-var userDepartment = prompt("What department are you looking for a course with in?")
+/* ---------------STUDENT GRADUATION & LEVEL ---------------*/
 
-while (userDepartment !== null) {
-  console.log(userDepartment);
-  if(userDepartment == "Transfiguration" || userDepartment == "Potions" || userDepartment == "Dark Arts"){
-    var userDepartmentFilter = filterDepartment(allCourses, userDepartment);
-    alert(userDepartmentFilter);
-    userDepartment = null;
+var gradYear = prompt("What is your expected graduation year?");
+var gradMonth = prompt("What month will you graduate?");
+var date = new Date();
+var currentYear = date.getFullYear();
+var currentMonth = date.getMonth();
+
+var welcomeCollegeStudent = function(level) {
+    alert("Welcome " + level + " !");
+  };
+
+var welcomeHsStudent = function(level) {
+  alert("You're still a " + level + " in high school!");
+  };
+
+var welcomeYoungStudent = function(level) {
+    alert("Whoa, college is some years away");
+    };
+
+var gradDate = function(month, year){
+  console.log(month + " " + year);
+  return month + " " + year;
+};
+
+var classLevel = function (month, year) {
+  console.log(currentYear);
+  var className = "";
+  if(month === "december"){ //if december
+    if(currentMonth>4){ //if month
+      var yearNum = year - currentYear;
+      console.log(yearNum);
+      if(yearNum == 0 || yearNum == 4){
+        className="Senior";
+      }
+      else if(yearNum == 1 || yearNum == 5){
+        className = "Junior";
+      }
+      else if(yearNum == 2 || yearNum == 6) {
+        className = "Sophmore";
+      }
+      else if(yearNum == 3  || yearNum == 7 ) {
+        className = "Freshman";
+      }
+    } //end month
+    else if(currentMonth < 4){ //else if month
+      var yearNum = (year-1) - currentYear;
+      console.log(yearNum);
+      if(yearNum == 0 || yearNum == 4){
+        className="Senior";
+      }
+      else if(yearNum == 1 || yearNum == 5){
+        className = "Junior";
+      }
+      else if(yearNum == 2 || yearNum == 6) {
+        className = "Sophmore";
+      }
+      else if(yearNum == 3  || yearNum == 7 ) {
+        className = "Freshman";
+      }
+      else {
+        className = "You are too young to be in high school or college!"
+      }
+    }// end else if month
+  }// end if december
+
+  else if(month === "may"){ //if else may
+    if(currentMonth<6){
+      var yearNum = year - currentYear;
+      if(yearNum == 0 || yearNum == 4){
+        className="Senior";
+      }
+      else if(yearNum == 1 || yearNum == 5){
+        className = "Junior";
+      }
+      else if(yearNum == 2 || yearNum == 6) {
+        className = "Sophmore";
+      }
+      else if(yearNum == 3  || yearNum == 7 ) {
+        className = "Freshman";
+      }
+    } //end if month
+    else if(currentMonth > 4){
+      var yearNum = (year-1) - currentYear;
+      console.log(yearNum);
+      if(yearNum == 0 || yearNum == 4){
+        className="Senior";
+      }
+      else if(yearNum == 1 || yearNum == 5){
+        className = "Junior";
+      }
+      else if(yearNum == 2 || yearNum == 6) {
+        className = "Sophmore";
+      }
+      else if(yearNum == 3  || yearNum == 7 ) {
+        className = "Freshman";
+      }
+      else {
+        className = "You are too young to be in high school or college!"
+      }
+    } //else if
   }
   else {
-    userDepartment = prompt("We do not have courses within the department. Try another department.");
+    prompt("Please choose either may or december for graduation month");
+  } //end else
+  console.log(className);
+  return className;
+}; //function close
+
+function welcomeStudentsByGraduatingClass(month, year) {
+  if(month !== null && year !==null){
+    var classResult = classLevel(gradMonth, gradYear);
+    if(year-currentYear < 4 && year > currentYear){
+      welcomeCollegeStudent(classResult);
+    }
+    else if (year-currentYear <= 8  && year-currentYear >= 4){
+      welcomeHsStudent(classResult);
+    }
+    else if(year-currentYear > 8){
+      welcomeYoungStudent(classResult);
+    }
+    else if(year < currentYear) {
+      alert("You are too old");
+    }
   }
-}
+  else{
+    prompt("Please start over.");
+  }
+};
+
+welcomeStudentsByGraduatingClass(gradMonth, gradYear);
